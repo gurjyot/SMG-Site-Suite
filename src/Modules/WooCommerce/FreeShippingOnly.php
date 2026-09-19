ba49e5d51a557f0129e9dc1f755255083d845218
Gurjyot Singh <thegurjyot@gmail.com>
2026-09-19 19:16:45 +0530

    feat: add media and WooCommerce modules

---
+++ src/Modules/WooCommerce/FreeShippingOnly.php
@@ -0,0 +1,10 @@
+<?php
+namespace SMG\SiteSuite\Modules\WooCommerce;
+use SMG\WPFoundation\Contracts\ModuleInterface;
+final class FreeShippingOnly implements ModuleInterface {
+    public function register():void{add_filter('woocommerce_package_rates',[$this,'rates'],100,2);}
+    public function rates(array $rates,array $package):array{
+        $free=[];foreach($rates as $id=>$rate){if($rate instanceof WC_Shipping_Rate&&$rate->get_method_id()==='free_shipping')$free[$id]=$rate;}
+        return $free!==[]?$free:$rates;
+    }
+}
