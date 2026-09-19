f915e3aefa4bc65f63bcc8ce7dc34adeae5814de
Gurjyot Singh <thegurjyot@gmail.com>
2026-09-19 22:13:16 +0530

    refactor: make comment field removal project-specific

--- src/Modules/Content/RemoveCommentWebsiteField.php
+++ src/Modules/Content/RemoveCommentWebsiteField.php
@@ -1,7 +1,14 @@
 <?php
 namespace SMG\SiteSuite\Modules\Content;
+
 use SMG\WPFoundation\Contracts\ModuleInterface;
+
 final class RemoveCommentWebsiteField implements ModuleInterface {
-    public function register():void{add_filter('comment_form_default_fields',[$this,'fields']);}
-    public function fields(array $fields):array{unset($fields['url']);return $fields;}
+    public function register():void{
+        add_filter('comment_form_default_fields',[$this,'removeUrlField']);
+    }
+
+    public function removeUrlField(array $fields):array{
+        return array_diff_key($fields,['url'=>true]);
+    }
 }
