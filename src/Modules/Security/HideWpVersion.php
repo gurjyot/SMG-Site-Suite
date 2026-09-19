12200a74df7d3c99ac1e0ca033f6be9366a49be4
Gurjyot Singh <thegurjyot@gmail.com>
2026-09-19 22:03:26 +0530

    refactor: make HideWpVersion implementation project-native

--- src/Modules/Security/HideWpVersion.php
+++ src/Modules/Security/HideWpVersion.php
@@ -1,18 +1,31 @@
 <?php
 namespace SMG\SiteSuite\Modules\Security;
+
 use SMG\WPFoundation\Contracts\ModuleInterface;
+
 final class HideWpVersion implements ModuleInterface {
     public function register():void{
         remove_action('wp_head','wp_generator');
-        add_filter('the_generator','__return_empty_string');
-        add_filter('style_loader_src',[$this,'stripCoreVersion'],9999);
-        add_filter('script_loader_src',[$this,'stripCoreVersion'],9999);
+        add_filter('the_generator',[$this,'emptyGenerator']);
+
+        foreach(['style_loader_src','script_loader_src'] as $hook){
+            add_filter($hook,[$this,'removeCoreVersion'],9999);
+        }
     }
-    public function stripCoreVersion(string $src):string{
+
+    public function emptyGenerator():string{
+        return '';
+    }
+
+    public function removeCoreVersion(string $src):string{
         if($src===''||!str_contains($src,'ver='))return $src;
-        $version=(string)get_bloginfo('version');
+
         $query=(string)wp_parse_url($src,PHP_URL_QUERY);
         parse_str($query,$args);
-        return isset($args['ver'])&&(string)$args['ver']===$version?remove_query_arg('ver',$src):$src;
+
+        if(!isset($args['ver']))return $src;
+
+        $coreVersion=(string)get_bloginfo('version');
+        return (string)$args['ver']===$coreVersion?remove_query_arg('ver',$src):$src;
     }
 }
