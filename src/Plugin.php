28e9ac909a8a9312a24040ff2fdd50a4f871faab
Gurjyot Singh <thegurjyot@gmail.com>
2026-09-19 19:14:53 +0530

    refactor: add Foundation admin browser and Site Suite bootstrap

---
+++ src/Plugin.php
@@ -0,0 +1,14 @@
+<?php
+namespace SMG\SiteSuite;
+use SMG\WPFoundation\Admin\ModuleBrowser;use SMG\WPFoundation\Modules\DependencyChecker;use SMG\WPFoundation\Modules\ModuleInstaller;use SMG\WPFoundation\Modules\ModuleLoader;use SMG\WPFoundation\Modules\ModuleManager;use SMG\WPFoundation\Modules\ModuleStateStore;
+final class Plugin {
+    private static ?self $instance=null;
+    public static function instance():self{return self::$instance??=new self();}
+    public function boot():void{
+        $registry=RegistryFactory::make();$state=new ModuleStateStore('smg_site_suite_active_modules');$deps=new DependencyChecker();
+        (new ModuleInstaller($registry,$state,'smg_site_suite_installed'))->initializeDefaultsOnce();
+        $manager=new ModuleManager($registry,$state,$deps);$loader=new ModuleLoader($registry,$state,$deps);
+        add_action('plugins_loaded',[$loader,'loadActive'],5);
+        if(is_admin())(new ModuleBrowser($manager,['page_title'=>__('SMG Site Suite','smg-site-suite'),'menu_title'=>__('Site Suite','smg-site-suite'),'menu_slug'=>'smg-site-suite','asset_url'=>SMG_SITE_SUITE_URL.'vendor/smg-wp-foundation/assets','version'=>SMG_SITE_SUITE_VERSION]))->boot();
+    }
+}
