d29aebb896be117cc2c44565075533b26365ebc6
Gurjyot Singh <thegurjyot@gmail.com>
2026-09-19 20:48:18 +0530

    feat: register default-featured-image ownership

--- src/DataRegistryFactory.php
+++ src/DataRegistryFactory.php
@@ -61,6 +61,7 @@ final class DataRegistryFactory {
             ->register('hide-admin-bar-by-role','option','smg_site_suite_hide_admin_bar_roles','configuration')
             ->register('environment-indicator','option','smg_site_suite_environment_indicator','configuration')
             ->register('auto-featured-image','option','smg_site_suite_auto_featured_image','configuration')
+            ->register('default-featured-image','option','smg_site_suite_default_featured_image','configuration')
             ->register('auto-update-email-controls','option','smg_site_suite_update_emails','configuration')
             ->register('temporary-login','user_meta','_smg_site_suite_temp_login','generated')
             ->register('temporary-login','cron','smg_site_suite_temp_login_cleanup','generated')
