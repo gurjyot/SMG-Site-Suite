6a90298fad31bf2396a0f82bc3c328758710a6d3
Gurjyot Singh <thegurjyot@gmail.com>
2026-09-19 19:27:42 +0530

    fix: preserve non-core asset cache-busting versions

--- src/Modules/Security/HideWpVersion.php
+++ src/Modules/Security/HideWpVersion.php
@@ -5,11 +5,14 @@ final class HideWpVersion implements ModuleInterface {
     public function register():void{
         remove_action('wp_head','wp_generator');
         add_filter('the_generator','__return_empty_string');
-        add_filter('style_loader_src',[$this,'stripVersion'],9999);
-        add_filter('script_loader_src',[$this,'stripVersion'],9999);
+        add_filter('style_loader_src',[$this,'stripCoreVersion'],9999);
+        add_filter('script_loader_src',[$this,'stripCoreVersion'],9999);
     }
-    public function stripVersion(string $src):string{
-        if($src==='')return $src;
-        return remove_query_arg('ver',$src);
+    public function stripCoreVersion(string $src):string{
+        if($src===''||!str_contains($src,'ver='))return $src;
+        $version=(string)get_bloginfo('version');
+        $query=(string)wp_parse_url($src,PHP_URL_QUERY);
+        parse_str($query,$args);
+        return isset($args['ver'])&&(string)$args['ver']===$version?remove_query_arg('ver',$src):$src;
     }
 }
