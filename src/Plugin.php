32b1090a14cdcc2bcb7d7fec697b7d85e57bc326
Gurjyot Singh <thegurjyot@gmail.com>
2026-09-19 21:28:02 +0530

    feat: boot Checkout Blocks compatibility guard

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,6 +1,6 @@
 <?php
 namespace SMG\SiteSuite;
-use SMG\SiteSuite\Admin\ConfigurationsPage;
+use SMG\SiteSuite\Admin\ConfigurationsPage;use SMG\SiteSuite\Admin\CompatibilityGuard;
 use SMG\WPFoundation\Admin\ModuleBrowser;use SMG\WPFoundation\Modules\DependencyChecker;use SMG\WPFoundation\Modules\ModuleInstaller;use SMG\WPFoundation\Modules\ModuleLoader;use SMG\WPFoundation\Modules\ModuleManager;use SMG\WPFoundation\Modules\ModuleStateStore;
 final class Plugin {
     private static ?self $instance=null;
@@ -11,6 +11,7 @@ final class Plugin {
         $manager=new ModuleManager($registry,$state,$deps);$loader=new ModuleLoader($registry,$state,$deps);
         add_action('plugins_loaded',[$loader,'loadActive'],5);
         if(is_admin()){
+            (new CompatibilityGuard($registry,$state))->boot();
             (new ConfigurationsPage($manager))->boot();
             (new ModuleBrowser($manager,['page_title'=>__('SMG Site Suite','smg-site-suite'),'menu_title'=>__('Site Suite','smg-site-suite'),'menu_slug'=>'smg-site-suite','asset_url'=>SMG_SITE_SUITE_URL.'vendor/smg-wp-foundation/assets','version'=>SMG_SITE_SUITE_VERSION]))->boot();
         }
